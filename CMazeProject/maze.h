2098715c35e7e134d9a977da66362ac695f30273
ObamaCooking <83021190+ObamaCooking@users.noreply.github.com>
2021-05-04 18:08:54 +0300

    Added initmaze :smile:

---
+++ CMazeProject/maze.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <stdio.h>
+#pragma warning (disable:4996)
+char** InitMaze(char* mazeFilename, int mazeNum);
--- CMazeProject/program.c
+++ CMazeProject/program.c
@@ -1,4 +1,6 @@
+#include "maze.h"
 int main()
 {
+	InitMaze("C:/Users/user1/Documents/GitHub/C-Maze/Maze.txt",1);
 	return 0;
 }
